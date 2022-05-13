314b9ec8dcca030e439016fbce2ced25f4785619
Chirag Simkhada <chiragsimkhada@gmail.com>
2022-05-13 14:32:47 +0545

    Initial Commit

---
+++ index.js
@@ -0,0 +1,53 @@
+const express = require("express");
+const cors = require("cors");
+
+const userRoutes = require("./routes/userRoutes");
+const messageRoute = require("./routes/messagesRoute");
+const videoRoute = require("./routes/videoRoute");
+const socket = require("socket.io");
+require("./database/connection");
+
+const app = express();
+require("dotenv").config();
+
+app.use(cors());
+app.use(express.json());
+
+app.use("/api/auth", userRoutes);
+app.use("/api/messages", messageRoute);
+app.use("/api/video", videoRoute);
+
+const server = app.listen(process.env.PORT, () =>
+  console.log(`Server started on ${process.env.PORT}`)
+);
+
+const io = socket(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    credentials: true,
+  },
+});
+
+global.onlineUsers = new Map();
+
+io.on("connection", (socket) => {
+  global.chatSocket = socket;
+  socket.on("add-user", (userId) => {
+    onlineUsers.set(userId, socket.id);
+  });
+
+  socket.on("send-msg", (data) => {
+    const sendUserSocket = onlineUsers.get(data.to);
+    if (sendUserSocket) {
+      socket.to(sendUserSocket).emit("msg-recive", data.message);
+    }
+  });
+
+  socket.on("join-room", ({ roomId, userId }) => {
+    const clientsInRoom = io.in(roomId).allSockets();
+    if (!userId in clientsInRoom) {
+      socket.join(roomId);
+    }
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+  });
+});
